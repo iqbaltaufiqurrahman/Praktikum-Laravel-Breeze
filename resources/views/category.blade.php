08993d8be459aa87510d62ffc5fc08c4a1e7eae6
Muhammad Iqbal <146165645+Etherrz@users.noreply.github.com>
2025-04-16 21:54:40 +0700

    Update UI

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -5,11 +5,11 @@
 <div class="parent md-12 bg-light" id="content-area">
     <div class="row">
         <!--Table-->
-        <div class="col-md-6 mt-3">
+        <div class="col-md-6 mt-3 pb-5">
             <div class="card shadow">
                 <div class="card-header"><a class="btn btn-success mt-2 mb-2" href=""><i class="fas fa-plus"></i></a></div>
                     <div class="card-body">
-                        <table class="table table-hover">
+                        <table class="table table-bordered">
                             <thead>
                                 <tr>
                                     <th class="head">No.</th>
